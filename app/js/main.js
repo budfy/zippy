b7513b3d21592a2685b5802b8294ced8111756a4
budfy <serdyuk.ro@gmail.com>
2019-10-04 02:44:08 +0300

    Header 95% ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(function(){
 
+  $('.header__bg-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 1500,
+    fade: true,
+    cssEase: 'linear',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 5000
+  });
+
+  $(window).scroll(function() {
+    if($(this).scrollTop() >= window.innerHeight / 2) {
+        $('.header-top__inner').addClass('scrolled');
+    }
+    else{
+        $('.header-top__inner').removeClass('scrolled');
+    }
+});
 
 
   
