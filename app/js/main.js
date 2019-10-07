8474f73e83e91815758ed1c28ab9145450177ca6
budfy <serdyuk.ro@gmail.com>
2019-10-07 06:02:29 +0300

    Video section ready 100%

--- app/js/main.js
+++ app/js/main.js
@@ -34,7 +34,59 @@ $(function(){
     }, 1000);
   });
 
-  
+
+
+  var wow = new WOW(
+    {
+      boxClass:     'wow',      // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset:       300,          // distance to the element when triggering the animation (default is 0)
+      mobile:       true,       // trigger animations on mobile devices (default is true)
+      live:         true,       // act on asynchronously loaded content (default is true)
+      callback:     function(box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+      },
+      scrollContainer: null // optional scroll container selector, otherwise use window
+    }
+  );
+  wow.init();
 
   
+
+  $('.video__popup-link').magnificPopup({
+    type: 'inline',
+    alignTop:false
+  });
+
+  $('.video__slider').slick({
+    centerMode: false,
+    slidesToShow: 3,
+    infinite: true,
+    speed: 300,
+    centerMode: true,
+    variableWidth: true,
+    arrows:true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,8 @@ gulp.task('imagemin', function(){
 gulp.task('script', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/wowjs/dist/wow.js'
   ])
   .pipe(concat('libs.min.js'))
   .pipe(uglify())
@@ -65,7 +66,8 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.css',
     'node_modules/magnific-popup/dist/magnific-popup.css',
-    'node_modules/normalize-css/normalize.css'
+    'node_modules/normalize-css/normalize.css',
+    'node_modules/animate.css/animate.css'
   ])
   .pipe(concat('libs.min.css'))
   .pipe(cssmin())
